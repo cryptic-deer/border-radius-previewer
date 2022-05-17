5d83b00c9c767fb543624faa8f625443c7b4ab36
cryptic-deer <artist.tibi@gmail.com>
2022-05-17 19:59:26 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/** @format */
+
+const box = document.querySelector(".box");
+
+const topLeftInput = document.querySelector("#top-left");
+const toprightInput = document.querySelector("#top-rigt");
+const bottomRightInput = document.querySelector("#bottom-right");
+const bottomLeftInput = document.querySelector("#bottom-left");
